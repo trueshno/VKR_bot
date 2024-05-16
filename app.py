0228f5ab8187a53dae30a5e9d405e86b491e7650
trueshno <138562665+Nadia111111@users.noreply.github.com>
2024-05-16 20:03:18 +0300

    feat: editing navigation for navbar

--- app.py
+++ app.py
@@ -19,5 +19,16 @@ def index():
         histogram_data.append(row[0])
 
     return render_template("index.html", data=data, labels=json.dumps(labels), histogram_data=json.dumps(histogram_data))
+
+@app.route("/statistics")
+def statistics():
+    # Логика обработки запроса для страницы "Статистика"
+    return render_template("statistics.html")
+
+@app.route("/tools")
+def tools():
+    # Логика обработки запроса для страницы "Инструменты"
+    return render_template("tools/tools.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
