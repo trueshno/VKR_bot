0de0810aa8e912577d4acb960b5114650a490764
trueshno <138562665+Nadia111111@users.noreply.github.com>
2024-05-17 01:42:07 +0300

    refactor: change structure of the project

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 conn = sqlite3.connect('db/database.db', check_same_thread=False)
 cursor = conn.cursor()
 
+
 @app.route("/")
 def index():
-    cursor.execute("SELECT * FROM test")
+    cursor.execute("SELECT * FROM user_info")
     data = cursor.fetchall()
 
     labels = []
@@ -18,15 +19,21 @@ def index():
         labels.append(row[1])
         histogram_data.append(row[0])
 
-    return render_template("index.html", data=data, labels=json.dumps(labels), histogram_data=json.dumps(histogram_data))
+    total_users = len(data)
+
+    return render_template("index.html", data=data, labels=json.dumps(labels),
+                           histogram_data=json.dumps(histogram_data), total_users=total_users)
+
 
 @app.route("/statistics")
 def statistics():
     return render_template("statistics/statistics.html")
 
+
 @app.route("/tools")
 def tools():
     return render_template("tools/tools.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
