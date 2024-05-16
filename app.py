392dd79ed76cf04a061b66c7703b6824241cea7d
trueshno <138562665+Nadia111111@users.noreply.github.com>
2024-05-16 18:40:25 +0300

    feat: editing navbar

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def index():
         labels.append(row[1])
         histogram_data.append(row[0])
 
-    return render_template("test_results.html", data=data, labels=json.dumps(labels), histogram_data=json.dumps(histogram_data))
+    return render_template("index.html", data=data, labels=json.dumps(labels), histogram_data=json.dumps(histogram_data))
 if __name__ == '__main__':
     app.run(debug=True)
