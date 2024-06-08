c5a312763a4329dda07b75e3eb9996a57b01f174
trueshno <viktorya.pas@yandex.ru>
2024-06-08 04:02:23 +0300

    refactor: add test page

--- app.py
+++ app.py
@@ -23,24 +23,8 @@ def statistics():
     return render_template("statistics/statistics.html", data=data, labels=json.dumps(labels),
                            histogram_data=json.dumps(histogram_data), total_users=total_users)
 
-@app.route("/statistics")
-def statistics():
-    cursor.execute("SELECT * FROM user_info")
-    data = cursor.fetchall()
-
-    labels = []
-    histogram_data = []
-    for row in data:
-        labels.append(row[1])
-        histogram_data.append(row[0])
-
-    total_users = len(data)
-
-    return render_template("statistics/statistics-day.html", data=data, labels=json.dumps(labels),
-                           histogram_data=json.dumps(histogram_data), total_users=total_users)
-
-@app.route("/statistics")
-def statistics():
+@app.route("/statistics-week")
+def statistics_week():
     cursor.execute("SELECT * FROM user_info")
     data = cursor.fetchall()
 
@@ -55,8 +39,8 @@ def statistics():
     return render_template("statistics/statistics-week.html", data=data, labels=json.dumps(labels),
                            histogram_data=json.dumps(histogram_data), total_users=total_users)
 
-@app.route("/statistics")
-def statistics():
+@app.route("/statistics-month")
+def statistics_month():
     cursor.execute("SELECT * FROM user_info")
     data = cursor.fetchall()
 
@@ -71,12 +55,16 @@ def statistics():
     return render_template("statistics/statistics-month.html", data=data, labels=json.dumps(labels),
                            histogram_data=json.dumps(histogram_data), total_users=total_users)
 
-@app.route("/tools")
-def tools():
+@app.route("/test")
+def test():
+    return render_template("test/test_results.html")
+
+@app.route("/tools-change")
+def tools_change():
     return render_template("tools/tools-change.html")
 
-@app.route("/tools")
-def tools():
+@app.route("/tools-add")
+def tools_add():
     return render_template("tools/tools-add.html")
 
 if __name__ == '__main__':
