84baf004c3bec74ee4cd40ed60f548a5b4156fe2
trueshno <viktorya.pas@yandex.ru>
2024-06-08 03:45:22 +0300

    refactor: add full page navigation

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect('db/database.db', check_same_thread=False)
 cursor = conn.cursor()
 
 @app.route("/")
-def index():
+def statistics():
     cursor.execute("SELECT * FROM user_info")
     data = cursor.fetchall()
 
@@ -20,15 +20,64 @@ def index():
 
     total_users = len(data)
 
-    return render_template("index.html", data=data, labels=json.dumps(labels),
+    return render_template("statistics/statistics.html", data=data, labels=json.dumps(labels),
                            histogram_data=json.dumps(histogram_data), total_users=total_users)
+
 @app.route("/statistics")
 def statistics():
-    return render_template("statistics/statistics.html")
+    cursor.execute("SELECT * FROM user_info")
+    data = cursor.fetchall()
+
+    labels = []
+    histogram_data = []
+    for row in data:
+        labels.append(row[1])
+        histogram_data.append(row[0])
+
+    total_users = len(data)
+
+    return render_template("statistics/statistics-day.html", data=data, labels=json.dumps(labels),
+                           histogram_data=json.dumps(histogram_data), total_users=total_users)
+
+@app.route("/statistics")
+def statistics():
+    cursor.execute("SELECT * FROM user_info")
+    data = cursor.fetchall()
+
+    labels = []
+    histogram_data = []
+    for row in data:
+        labels.append(row[1])
+        histogram_data.append(row[0])
+
+    total_users = len(data)
+
+    return render_template("statistics/statistics-week.html", data=data, labels=json.dumps(labels),
+                           histogram_data=json.dumps(histogram_data), total_users=total_users)
+
+@app.route("/statistics")
+def statistics():
+    cursor.execute("SELECT * FROM user_info")
+    data = cursor.fetchall()
+
+    labels = []
+    histogram_data = []
+    for row in data:
+        labels.append(row[1])
+        histogram_data.append(row[0])
+
+    total_users = len(data)
+
+    return render_template("statistics/statistics-month.html", data=data, labels=json.dumps(labels),
+                           histogram_data=json.dumps(histogram_data), total_users=total_users)
 
 @app.route("/tools")
 def tools():
-    return render_template("tools/tools.html")
+    return render_template("tools/tools-change.html")
+
+@app.route("/tools")
+def tools():
+    return render_template("tools/tools-add.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
