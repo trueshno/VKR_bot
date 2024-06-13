786e0eb17b16457ec9982ac2888f504ced66eba9
trueshno <viktorya.pas@yandex.ru>
2024-06-13 03:34:30 +0300

    refactor: add statistics

--- app.py
+++ app.py
@@ -29,21 +29,27 @@ def statistics():
     return render_template("statistics/statistics.html", data=data, labels=json.dumps(labels),
                            histogram_data=json.dumps(histogram_data), total_users=total_users)
 
+
+import datetime
+
+
 @app.route("/statistics-week")
 def statistics_week():
-    cursor.execute("SELECT * FROM user_info")
+    # Получаем текущую дату
+    today = datetime.date.today()
+
+    # Вычисляем дату 7 дней назад
+    one_week_ago = today - datetime.timedelta(days=7)
+
+    # Формируем SQL-запрос для выборки данных за последнюю неделю
+    cursor.execute("SELECT DATE(created_at) AS date, COUNT(*) AS count FROM user_info WHERE created_at >= ? GROUP BY date ORDER BY date ASC", (one_week_ago,))
     data = cursor.fetchall()
 
-    labels = []
-    histogram_data = []
-    for row in data:
-        labels.append(row[1])
-        histogram_data.append(row[0])
-
-    total_users = len(data)
+    labels = [row[0] for row in data]
+    histogram_data = [row[1] for row in data]
 
     return render_template("statistics/statistics-week.html", data=data, labels=json.dumps(labels),
-                           histogram_data=json.dumps(histogram_data), total_users=total_users)
+                           histogram_data=json.dumps(histogram_data))
 
 @app.route("/statistics-month")
 def statistics_month():
